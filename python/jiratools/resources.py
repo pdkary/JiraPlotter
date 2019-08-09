fc166de924738919d48d9f926d2287b2d323741e
Parker Kary <parker.kary@fiixsoftware.com>
2019-08-09 09:17:15 -0400

    added missing quote

--- python/jiratools/resources.py
+++ python/jiratools/resources.py
@@ -1,5 +1,5 @@
 api_token = "YOUR_API_KEY"
-user_name = "YOUR_USERNAME
+user_name = "YOUR_USERNAME"
 server = "YOUR_ATLASSIAN_SUBDOMAIN"
 
 chart_endpoint = "/rest/greenhopper/1.0/rapid/charts/velocity.json?rapidViewId="
