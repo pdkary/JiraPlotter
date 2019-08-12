46568817c94cfe7609298780b1410973df9b0232
Parker Kary <parker.kary@fiixsoftware.com>
2019-08-12 17:56:13 -0400

    readme formatting

--- python/jiratools/resources.py
+++ python/jiratools/resources.py
@@ -1,5 +1,5 @@
 api_token = "YOUR_API_TOKEN"
-user_name = "YOUR_USER_NAME
+user_name = "YOUR_USER_NAME"
 server = "YOUR_ATLASSIAN_DOMAIN"
 
 chart_endpoint = "/rest/greenhopper/1.0/rapid/charts/velocity.json?rapidViewId="
