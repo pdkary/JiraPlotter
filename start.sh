5c8c0a555716a883dc8e8c9f5a5e8d908eab0839
Parker Kary <parker.kary@fiixsoftware.com>
2019-08-22 18:04:56 -0400

    added venv link to python start

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 start_python () {
     cd python
+    source venv/bin/activate
     python start.py
 }
 start_ui () {
     cd web
     yarn start
 }
-
 kill $(lsof -t -i:5000)
 kill $(lsof -t -i:4200)
 start_python & start_ui
